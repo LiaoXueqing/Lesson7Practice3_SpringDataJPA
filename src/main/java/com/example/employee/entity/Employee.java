a72496d5dbbb4cf24756411fcbdaceb89b52199f
xuexue <1047504737@qq.com>
2018-05-07 21:28:02 +0800

    updating

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,16 @@
 package com.example.employee.entity;
+import javax.persistence.Entity;
 
+@Entity
 public class Employee {
+    @Id
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer companyId;
+    private Integer salary;
+
+
 
 }
