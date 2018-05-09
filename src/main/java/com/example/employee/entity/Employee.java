d82a0e001a89490daa707b0ac32518c5d4cb9277
xuexue <1047504737@qq.com>
2018-05-09 10:15:54 +0800

    finish

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.example.employee.entity;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Employee {
@@ -11,6 +12,61 @@ public class Employee {
     private Integer companyId;
     private Integer salary;
 
+    public Employee(String name, Integer age, String gender, Integer companyId, Integer id, Integer salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+    public Employee(){}
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
 }
